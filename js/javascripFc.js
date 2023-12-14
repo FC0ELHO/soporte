9f0b59d832f9aa6c4ce3670e15ea644dedff24be
FC0ELHO <facp84@gmail.com>
2023-12-14 15:43:46 -0300

    prueba 2

--- js/javascripFc.js
+++ js/javascripFc.js
@@ -6,3 +6,21 @@ function copiarAlPortapapeles(id_elemento) {
     document.execCommand("copy");
     document.body.removeChild(aux);
   }
+
+
+  function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
+
+  
